fbc6505b0e6be94bc474fd200b13ca618bee8a02
Edmilton <edmiltonsouzamsc@gmail.com>
2024-06-04 02:30:30 -0300

    Site Pronto e adicionada responsividade

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtém o botão de contato
+        var btnContato = document.querySelector('.btn-contato button');
+
+        // Adiciona um evento de clique ao botão de contato
+        btnContato.addEventListener('click', function() {
+            // URL do seu WhatsApp com o número de telefone
+            var whatsappURL = 'https://wa.me/+5581988331887';
+
+            // Abre uma nova guia com o link do WhatsApp
+            window.open(whatsappURL, '_blank');
+        });
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        // Obtém o botão "Entre em Contato"
+        var btnEntreEmContato = document.querySelector('.topo-do-site .btn-contato button');
+
+        // Adiciona um evento de clique ao botão "Entre em Contato"
+        btnEntreEmContato.addEventListener('click', function() {
+            // URL do seu WhatsApp com o número de telefone
+            var whatsappURL = 'https://wa.me/+5581988331887';
+
+            // Abre uma nova guia com o link do WhatsApp
+            window.open(whatsappURL, '_blank');
+        });
+    });
+
+
+
+
+
